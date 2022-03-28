9ea36308b5e815b6424d4cce616d9c15af3034c3
Lucas Felipe <lucasfs1007@gmail.com>
2022-03-28 19:19:42 -0300

    Primeiro commit

---
+++ src/Components/Calculator.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Calculator.css'
+import Container from '@mui/material/Container';
+import { Box } from "@mui/system";
+
+export default function Calculator(){
+    return(
+        <div>
+            <Box m={5} />
+            <Container maxWidth="xs">
+                 <div className='fundoTela'>
+                    {/* Criacao dos botoes que vou usar */}
+                    <button> Limpar </button> 
+                    <button> +/- </button> 
+                    <button> % </button> 
+                    <button className='Laranja'> /</button> 
+                    <button className='Cinza'> 7</button> 
+                    <button className='Cinza'> 8 </button> 
+                    <button className='Cinza'> 9 </button> 
+                    <button className='Laranja'> X </button> 
+                    <button className='Cinza'> 4 </button> 
+                    <button className='Cinza'> 5</button> 
+                    <button className='Cinza'> 6</button> 
+                    <button className='Laranja'> - </button> 
+                    <button className='Cinza'> 1 </button> 
+                    <button className='Cinza'> 2 </button> 
+                    <button className='Cinza'> 3 </button> 
+                    <button className='Laranja'> + </button>
+                    <button className='Cinza'> 0 </button> 
+                    <button className='Cinza'> , </button> 
+                    <button> = </button> 
+                </div>
+        </Container>
+        </div>
+       
+    )  
+}
