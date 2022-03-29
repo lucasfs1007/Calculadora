29497f4b7354f5955469f21d3e8a2d1940bb50c4
Lucas Felipe <lucasfs1007@gmail.com>
2022-03-28 22:18:11 -0300

    Quase finalizando o design da calculadora

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -5,33 +5,34 @@ import { Box } from "@mui/system";
 
 export default function Calculator(){
     return(
-        <div>
-            <Box m={5} />
-            <Container maxWidth="xs">
-                 <div className='fundoTela'>
-                    {/* Criacao dos botoes que vou usar */}
-                    <button> Limpar </button> 
-                    <button> +/- </button> 
-                    <button> % </button> 
-                    <button className='Laranja'> /</button> 
-                    <button className='Cinza'> 7</button> 
-                    <button className='Cinza'> 8 </button> 
-                    <button className='Cinza'> 9 </button> 
-                    <button className='Laranja'> X </button> 
-                    <button className='Cinza'> 4 </button> 
-                    <button className='Cinza'> 5</button> 
-                    <button className='Cinza'> 6</button> 
-                    <button className='Laranja'> - </button> 
-                    <button className='Cinza'> 1 </button> 
-                    <button className='Cinza'> 2 </button> 
-                    <button className='Cinza'> 3 </button> 
-                    <button className='Laranja'> + </button>
-                    <button className='Cinza'> 0 </button> 
-                    <button className='Cinza'> , </button> 
-                    <button> = </button> 
-                </div>
-        </Container>
-        </div>
+       <div>
+           <Box m={5} />
+           <Container maxWidth="xs">
+               <div className="fundoTela">
+                   <h1 className='resultado'>1</h1>
+                   <button>AC</button>
+                   <button>+/-</button>
+                   <button>%</button>
+                   <button className='Laranja'>/</button>
+                   <button className='Cinza'>7</button>
+                   <button className='Cinza'>8</button>
+                   <button className='Cinza'>9</button>
+                   <button className='Laranja'>X</button>
+                   <button className='Cinza'>4</button>
+                   <button className='Cinza'>5</button>
+                   <button className='Cinza'>6</button>
+                   <button className='Laranja'>-</button>
+                   <button className='Cinza'>1</button>
+                   <button className='Cinza'>2</button>
+                   <button className='Cinza'>3</button>
+                   <button className='Laranja'>+</button>
+                   <button className='Cinza'>0</button>
+                   <button className='Cinza'>,</button>
+                   <button className='Cinza'style={{visibility:"hidden"}}>,</button>
+                   <button>=</button>
+               </div>
+           </Container>
+       </div>
        
     )  
 }
