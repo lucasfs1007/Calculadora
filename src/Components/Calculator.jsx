4699bde0742d5ffbeda8478f980df93ce9f41b3c
Lucas Felipe <lucasfs1007@gmail.com>
2022-03-29 14:03:52 -0300

    Finalizando design

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -10,26 +10,37 @@ export default function Calculator(){
            <Container maxWidth="xs">
                <div className="fundoTela">
                    <h1 className='resultado'>1</h1>
-                   <button>AC</button>
-                   <button>+/-</button>
-                   <button>%</button>
-                   <button className='Laranja'>/</button>
-                   <button className='Cinza'>7</button>
-                   <button className='Cinza'>8</button>
-                   <button className='Cinza'>9</button>
-                   <button className='Laranja'>X</button>
-                   <button className='Cinza'>4</button>
-                   <button className='Cinza'>5</button>
-                   <button className='Cinza'>6</button>
-                   <button className='Laranja'>-</button>
-                   <button className='Cinza'>1</button>
-                   <button className='Cinza'>2</button>
-                   <button className='Cinza'>3</button>
-                   <button className='Laranja'>+</button>
-                   <button className='Cinza'>0</button>
-                   <button className='Cinza'>,</button>
-                   <button className='Cinza'style={{visibility:"hidden"}}>,</button>
-                   <button>=</button>
+                   <div>
+                    <button>AC</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                    <button className='Laranja'>/</button>
+                   </div>
+                   <div>
+                    <button className='Cinza'>7</button>
+                    <button className='Cinza'>8</button>
+                    <button className='Cinza'>9</button>
+                    <button className='Laranja'>X</button>
+                   </div>
+                   <div>
+                    <button className='Cinza'>4</button>
+                    <button className='Cinza'>5</button>
+                    <button className='Cinza'>6</button>
+                    <button className='Laranja'>-</button>
+                   </div>
+                   <div>
+                    <button className='Cinza'>1</button>
+                    <button className='Cinza'>2</button>
+                    <button className='Cinza'>3</button>
+                    <button className='Laranja'>+</button>
+                   </div>
+                   <div>
+                    <button className='Cinza'>0</button>
+                    <button className='Cinza'>,</button>
+                    <button className='Cinza'style={{visibility:"hidden"}}>,</button>
+                    <button>=</button>
+                   </div>
+                   
                </div>
            </Container>
        </div>
